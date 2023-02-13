01bb776250595d8b4021ec82685f2f4cb6b3edfa
JooelMan <jooel.mantymaa@wippies.com>
2023-02-13 14:51:30 +0200

    add request body to logging

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -3,8 +3,10 @@ const morgan = require('morgan')
 
 const app = express()
 
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
   {
