5b406b85ea2c5e931a4dcf09256d16d7ae56af2f
JooelMan <jooel.mantymaa@wippies.com>
2023-02-13 11:16:32 +0200

    add functionality to delete single phone numbers

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -28,9 +28,15 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)  
+  persons = persons.filter(p => p.id !== id)
+  
+  res.status(204).end()
+})
+
 app.get('/info', (req, res) => {
   const date = new Date()
-  console.log(date);
   res.send(`<p>Phonebook has info for 2 people</p><p>${date}</p>`)
 })
 
