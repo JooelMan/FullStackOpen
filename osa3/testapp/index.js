bd63a22c2eccc929b91055d762fcf9c1bc34edb4
JooelMan <jooel.mantymaa@wippies.com>
2023-02-23 11:16:55 +0200

    add frontend production build

--- osa3/testapp/index.js
+++ osa3/testapp/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
   {
@@ -73,6 +77,7 @@ app.delete('/api/notes/:id', (request, response)  => {
   response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
