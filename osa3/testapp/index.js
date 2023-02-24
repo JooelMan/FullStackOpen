54731182643caaf9d449e348979b86d1ba49f292
JooelMan <jooel.mantymaa@wippies.com>
2023-02-24 16:54:40 +0200

    add mongo.js and dotenv

--- osa3/testapp/index.js
+++ osa3/testapp/index.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note')
 
 const app = express()
 app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
 
 let notes = [
   {
@@ -23,14 +26,14 @@ let notes = [
   }
 ]
 
-app.use(express.json())
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -44,30 +47,23 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
-    id: generateId(),
   }
   
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (request, response)  => {
@@ -77,7 +73,7 @@ app.delete('/api/notes/:id', (request, response)  => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
