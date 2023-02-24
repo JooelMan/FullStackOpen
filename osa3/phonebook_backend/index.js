b529c41749339609f4860a80359c65bc016ea721
JooelMan <jooel.mantymaa@wippies.com>
2023-02-24 16:54:07 +0200

    add database functionality to backend

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./modules/person.js')
 
 const app = express()
 
@@ -11,27 +13,18 @@ app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Proffa",
-    number: "0404044444"
-  },
-  {
-    id: 2,
-    name: "Keke",
-    number: "1234567890"
-  }
-]
+//const generateID = () => {
+//  min = 0
+//  max = 1000
+//  return Math.floor(Math.random() * (max - min + 1) + min)
+//}
 
-const generateID = () => {
-  min = 0
-  max = 1000
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+let persons = []
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -40,36 +33,27 @@ app.post('/api/persons', (req, res) => {
   
   
   if (!body.name || !body.number) {
-    return res.status(400).json({ 
-      error: 'name or number missing' 
-    })
+    return res.status(400).json({ error: 'name or number missing' })
   }
 
-  if (persons.find(p => p.name === body.name)) {
-    return res.status(400).json({ 
-      error: 'name already in use' 
-    })
-  }
+  //if (persons.find(p => p.name === body.name)) {
+  //  return res.status(400).json({ error: 'name already in use' })
+  //}
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateID(),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  res.json(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
+  Person.findById(req.params.id).then(person => {
     res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
