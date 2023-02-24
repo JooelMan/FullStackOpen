eb9dabe5208f79d43a686569a34c7e26e31b4ab2
JooelMan <jooel.mantymaa@wippies.com>
2023-02-24 11:00:30 +0200

    uibuild

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -7,8 +7,8 @@ const app = express()
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
 app.use(cors())
-app.use(express.static('build'))
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
