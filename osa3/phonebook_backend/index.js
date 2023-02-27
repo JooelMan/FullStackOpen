555d12fe360ee118d3328cbe94ba076d780bd6cc
JooelMan <jooel.mantymaa@wippies.com>
2023-02-27 18:42:28 +0200

    Add eslint

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -14,86 +14,86 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(express.static('build'))
 
 app.get('/info', (req, res, next) => {
-  const date = new Date()
-  Person.find({})
-    .then(people => {
-      res.send(`<p>Phonebook has info for ${people.length} people</p><p>${date}</p>`)
-    })
-    .catch(error => next(error))
+	const date = new Date()
+	Person.find({})
+		.then(people => {
+			res.send(`<p>Phonebook has info for ${people.length} people</p><p>${date}</p>`)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons', (req, res, next) => {
-  Person.find({}).then(people => {
-    res.json(people)
-  })
+	Person.find({}).then(people => {
+		res.json(people)
+	})
 })
 
 app.post('/api/persons', (req, res, next) => {
-  const body = req.body
+	const body = req.body
 
-  //if (persons.find(p => p.name === body.name)) {
-  //  return res.status(400).json({ error: 'name already in use' })
-  //}
+	//if (persons.find(p => p.name === body.name)) {
+	//  return res.status(400).json({ error: 'name already in use' })
+	//}
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
 
-  person.save()
-    .then(savedPerson => {
-      res.json(savedPerson)
-    })
-    .catch(error => next(error))
+	person.save()
+		.then(savedPerson => {
+			res.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then(person => {
-      res.json(person)
-    })
-    .catch(error => next(error))
+	Person.findById(req.params.id)
+		.then(person => {
+			res.json(person)
+		})
+		.catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
+	Person.findByIdAndRemove(req.params.id)
+		.then(result => {
+			res.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
+	const body = req.body
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
-    .then(updatedPerson => {
-      res.json(updatedPerson)
-    })
-    .catch(error => next(error))
+	Person.findByIdAndUpdate(req.params.id, person, { new: true })
+		.then(updatedPerson => {
+			res.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 
 // olemattomien osoitteiden käsittely
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
 
-  next(error)
+	next(error)
 }
 
 // tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
@@ -101,5 +101,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
