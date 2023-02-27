9ec4f346e30d3fea3d6e6ed44edbf1e0894952e1
JooelMan <jooel.mantymaa@wippies.com>
2023-02-27 13:06:56 +0200

    Add number update functionality

--- osa3/phonebook_backend/index.js
+++ osa3/phonebook_backend/index.js
@@ -21,7 +21,7 @@ app.use(express.static('build'))
 
 let persons = []
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   Person.find({}).then(people => {
     res.json(people)
   })
@@ -60,6 +60,20 @@ app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
     })
     .catch(error => next(error))
 })
